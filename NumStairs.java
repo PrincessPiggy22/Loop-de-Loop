8a36b55d8e2805ec7d889cbe2fec0ef6e47bd715
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-09-16 10:16:30 -0400

    Create NumStairs.java

---
+++ NumStairs.java
@@ -0,0 +1,12 @@
+// Number Stairs
+
+public class NumStairs {
+    public static void main(String[] args) {
+        // Nested loops
+        // end at 12345678910
+        // use list/array?
+        for (int i = 0; i == 10; i++){
+
+        }
+    }
+}
