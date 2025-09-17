adcf7c4dcd870bd2e46b450623bc7a580e03c734
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-09-17 09:04:22 -0400

    Update NumStairs.java

--- NumStairs.java
+++ NumStairs.java
@@ -1,12 +1,40 @@
-// Number Stairs
-
 public class NumStairs {
     public static void main(String[] args) {
         // Nested loops
         // end at 12345678910
         // use list/array?
-        for (int i = 0; i == 10; i++){
-
+        System.out.println("start");
+        int number = 1;
+        for (int i = 0; i < 1; i++){
+            System.out.println(1);
+            for (int j = 0; j < 1; j++) {
+                System.out.println(12);
+                for (int k = 0; k < 1 ; k++) {
+                    System.out.println(123);
+                    for (int l = 0; l < 1; l++) {
+                        System.out.println(1234);
+                        for (int m = 0; m < 1; m++) {
+                            System.out.println(12345);
+                            for (int n = 0; n < 1; n++) {
+                                System.out.println(123456);
+                                for (int o = 0; o < 1; o++) {
+                                    System.out.println(1234567);
+                                    for (int p = 0; p < 1; p++) {
+                                        System.out.println(12345678);
+                                        for (int q = 0; q < 1; q++) {
+                                            System.out.println(123456789);
+                                            for (int r = 0; r < 1; r++) {
+                                                System.out.println(123456789 + "10"); // Integer number was to large
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
+        System.out.println("\nend");
     }
 }
